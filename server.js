ec33a5c2bb40f3477f969df0b70692e20cdb02bc
Ariel Weiss <rel@Ariels-MacBook-Pro.local>
2015-09-21 11:43:37 +0300

    updated support for SAT

--- server.js
+++ server.js
@@ -3,30 +3,22 @@ var deeplink = require('node-deeplink')
  
 var app = express();
  
-app.get('/', deeplink({ 
-    fallback: 'http://zinkerz.com',
-    android_package_name: 'com.citylifeapps.cups', 
+app.get('/toefl', deeplink({ 
+    fallback: 'https://zinkerz.com/toefl',
+    android_package_name: 'com.zinkerz.zinkerztoefl', 
     ios_store_link: 'https://itunes.apple.com/us/app/zinkerz-toefl-full-practice/id1014499280?ls=1&mt=8'
 }));
 
-app.get('/deeplink',  function(request,response) {
-	deeplink({ 
-	    fallback: 'http://zinkerz.com',
-	    android_package_name: 'com.citylifeapps.cups', 
-	    ios_store_link: 'https://itunes.apple.com/us/app/zinkerz-toefl-full-practice/id1014499280?ls=1&mt=8'
-	});
-});
+app.get('/sat', deeplink({ 
+    fallback: 'http://zinkerz.com/sat',
+    android_package_name: 'com.zinkerz.zinkerzsat', 
+    ios_store_link: 'https://itunes.apple.com/us/app/zinkerz-sat-full-practice/id955488739?ls=1&mt=8'
+}));
 
-app.get('/test', function(request,response){
-	console.log("Working...");
-	response.send('Hello');
-});
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
+// app.get('/test', function(request,response){
+// 	console.log("Working...");
+// 	response.send('Hello');
+// });
 
 app.set('port', (process.env.PORT || 5000));
 
