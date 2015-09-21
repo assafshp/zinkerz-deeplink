196c378cb89defece9fc2c52afec707b64c4f59c
Ariel Weiss <rel@Ariels-MacBook-Pro.local>
2015-09-21 11:20:40 +0300

    1

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ var deeplink = require('node-deeplink')
 var app = express();
  
 app.get('/deeplink', deeplink({ 
-    fallback: 'https://cupsapp.com',
+    fallback: 'http://zinkerz.com',
     android_package_name: 'com.citylifeapps.cups', 
-    ios_store_link: 'https://itunes.apple.com/us/app/zinkerz-toefl-full-practice/id1014499280?ls=1&mt=8',
+    ios_store_link: 'https://itunes.apple.com/us/app/zinkerz-toefl-full-practice/id1014499280?ls=1&mt=8'
 }));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
