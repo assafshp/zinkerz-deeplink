5b03a837bd2e5b811c6e1719714440779c8d9066
assafshp <assafshp@zahav.net.il>
2015-11-01 14:26:09 +0200

    added sat/

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # zinkerz-deeplink
+
+Examples
+==========
+```
+http://localhost:5000/sat?url=mobilezinkerzsat%3A%2F%2F
+http://localhost:5000/sat/purchase?url=mobilezinkerzsat%3A%2F%2F
+
+http://localhost:5000/toefl?url=mobilezinkerztoefl%3A%2F%2F
+http://localhost:5000/toefl/purchase?url=mobilezinkerztoefl%3A%2F%2F
+
+http://localhost:5000
+
+```
--- server.js
+++ server.js
@@ -2,23 +2,40 @@ var express = require('express');
 var deeplink = require('node-deeplink')
  
 var app = express();
- 
-app.get('/toefl', deeplink({ 
-    fallback: 'http://zinkerz.com/toefl',
-    android_package_name: 'com.zinkerz.zinkerztoefl', 
-    ios_store_link: 'https://itunes.apple.com/us/app/zinkerz-toefl-full-practice/id1014499280?ls=1&mt=8'
-}));
 
-app.get('/sat', deeplink({ 
+var extend = function extend(target) {
+    var sources = [].slice.call(arguments, 1);
+    sources.forEach(function (source) {
+        for (var prop in source) {
+            target[prop] = source[prop];
+        }
+    });
+    return target;
+}
+
+var sat_options = { 
     fallback: 'http://zinkerz.com/sat',
     android_package_name: 'com.zinkerz.zinkerzsat', 
     ios_store_link: 'https://itunes.apple.com/us/app/zinkerz-sat-full-practice/id955488739?ls=1&mt=8'
-}));
+}
 
-// app.get('/test', function(request,response){
-// 	console.log("Working...");
-// 	response.send('Hello');
-// });
+var toefl_options = { 
+    fallback: 'http://zinkerz.com/toefl',
+    android_package_name: 'com.zinkerz.zinkerztoefl', 
+    ios_store_link: 'https://itunes.apple.com/us/app/zinkerz-toefl-full-practice/id1014499280?ls=1&mt=8'
+}
+
+
+app.get('/toefl/purchase', deeplink(extend(toefl_options, {title: 'purchase'})));
+app.get('/toefl', deeplink(toefl_options));
+
+app.get('/sat/purchase', deeplink(extend(sat_options,{title: 'purchase'})));
+app.get('/sat', deeplink(sat_options));
+
+app.get('/*', function (req, res, next) {
+		console.log('all');
+		res.redirect('http://zinkerz.com');
+});
 
 app.set('port', (process.env.PORT || 5000));
 
